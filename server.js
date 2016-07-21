d1baf407e56b0b8617c3dbae382c41a01388dcfc
ValeryYafremau <vaefremov.gomel@gmail.com>
2016-07-21 18:38:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ Imap = require('imap');
 _getEmail = function(flag, condition) {
   var fetchEmail, imap, maxLatency, res, start, sync;
   imap = new Imap({
-    user: 'dezmandpalmusgermany@gmail.com',
-    password: 'inokkoeayegveigm',
+    user: 'autotest.cms.scoop@gmail.com',
+    password: 'js4playback',
     host: 'imap.gmail.com',
     port: 993,
     tls: true
